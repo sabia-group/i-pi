850fb8e7b909339c0395ef43695347e16f03b569
Vahideh Alizadeh <vahideh.90.alizadeh@gmail.com>
2025-09-10 12:08:13 +0200

    update frictiontools

--- ipi/utils/frictiontools.py
+++ ipi/utils/frictiontools.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import numpy as np
 from scipy.integrate import quad
 
@@ -18,3 +20,28 @@ def get_alpha(eta0: float, omega_cutoff: float, nm: NormalModes) -> np.ndarray:
         # Integrate from 0 to infinity
         alpha[idx] = 2 / np.pi * quad(Value, 0, np.inf)[0]
     return alpha
+
+def expohmic_J(omega: Union[float, np.ndarray], eta, omega_cut) -> Union[float, np.ndarray]:
+        """Spectral density at frequency omega.
+        """
+        return eta * np.abs(omega) * np.exp(-np.abs(omega)/omega_cut)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="tool to compute the spectral densities for the electronic friction")
+    parser.add_argument("--omega-cut", required=True, type=float)
+    parser.add_argument("--eta", required=True, type=float)
+    parser.add_argument("output", type=str)
+    args = parser.parse_args()
+
+    omega_cut = args.omega_cut
+    
+    eta = args.eta
+    omega = np.arange(omega_cut)
+
+    J = expohmic_J(omega, eta, omega_cut)
+
+    print(np.stack((omega, J)))
+    with open(args.output, "w") as fd:
+         for i in range(J.size):
+              fd.write(f"{omega[i]} {J[i]}\n")
