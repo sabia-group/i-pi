c3bae6880334da4436e1e25c161034a978e23fd1
Vahideh Alizadeh <vahideh.90.alizadeh@gmail.com>
2025-09-10 12:12:53 +0200

    make pretty and lint

--- ipi/utils/frictiontools.py
+++ ipi/utils/frictiontools.py
@@ -21,21 +21,27 @@ def get_alpha(eta0: float, omega_cutoff: float, nm: NormalModes) -> np.ndarray:
         alpha[idx] = 2 / np.pi * quad(Value, 0, np.inf)[0]
     return alpha
 
-def expohmic_J(omega: Union[float, np.ndarray], eta, omega_cut) -> Union[float, np.ndarray]:
-        """Spectral density at frequency omega.
-        """
-        return eta * np.abs(omega) * np.exp(-np.abs(omega)/omega_cut)
+
+def expohmic_J(
+    omega: Union[float, np.ndarray], eta, omega_cut
+) -> Union[float, np.ndarray]:
+    """Spectral density at frequency omega."""
+    return eta * np.abs(omega) * np.exp(-np.abs(omega) / omega_cut)
+
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description="tool to compute the spectral densities for the electronic friction")
+
+    parser = argparse.ArgumentParser(
+        description="tool to compute the spectral densities for the electronic friction"
+    )
     parser.add_argument("--omega-cut", required=True, type=float)
     parser.add_argument("--eta", required=True, type=float)
     parser.add_argument("output", type=str)
     args = parser.parse_args()
 
     omega_cut = args.omega_cut
-    
+
     eta = args.eta
     omega = np.arange(omega_cut)
 
@@ -43,5 +49,5 @@ if __name__ == "__main__":
 
     print(np.stack((omega, J)))
     with open(args.output, "w") as fd:
-         for i in range(J.size):
-              fd.write(f"{omega[i]} {J[i]}\n")
+        for i in range(J.size):
+            fd.write(f"{omega[i]} {J[i]}\n")
